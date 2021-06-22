07940e90cb1e99b65dc388ba8c63442b56873e53
Mahathi1992 <mtb.violin@gmail.com>
2021-06-22 17:58:09 +0200

    added laser2pointcloud node

--- docker/ros_melodic_desktop_px4/run_no_nvidia.sh
+++ docker/ros_melodic_desktop_px4/run_no_nvidia.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
 #check if there is a /build and /devel in the current PWD
-# if [ -f devel/.catkin ]; then
-# echo "Starting from Catkin Workspace: $PWD, preparing it's ROS_PACKAGE_PATH."
+#if [ -f devel/.catkin ]; then
+#echo "Starting from Catkin Workspace: $PWD, preparing it's ROS_PACKAGE_PATH."
 
 # # If you start this script inside a catkin workspace, it will make that workspace available in the projects src folder
-# sed -i 's/;\/projects\/src//' devel/.catkin
-# echo -n "`cat devel/.catkin`;/projects/src" > devel/.catkin
-# fi
+#sed -i 's/;\/projects\/src//' devel/.catkin
+#echo -n "`cat devel/.catkin`;/projects/src" > devel/.catkin
+#fi
 
 docker run -iPt \
     --rm \
     --env="DISPLAY=$DISPLAY" \
     --env="QT_X11_NO_MITSHM=1" \
     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-    #--net=host \
+    --net=host \
+    --privileged \
     --volume="$XAUTHORITY:$XAUTHORITY" \
     --device=/dev/dri \
     --env="XAUTHORITY=$XAUTHORITY" \
